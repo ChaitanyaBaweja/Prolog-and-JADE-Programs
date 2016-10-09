2d35716e94d8b2184cfdfc94cf1e9f99c40fb27a
ChaitanyaBaweja <chaitanya.baweja1@gmail.com>
2016-10-09 20:26:51 +0530

    Add files via upload

--- Prolog/Prob 6.pl
+++ Prolog/Prob 6.pl
@@ -1,5 +1,6 @@
 /* Program to reverse any given number.
    Copyright (c) 2016 ChaitanyaBaweja
 */
-/* proposed method is to covert the number into a list, reverse the list, convert it back into a number.
-*/
+rev(N,R):-rev(N,0,R).
+rev(0,R,R):-!.
+rev(N,A,R):- A1 is mod(N,10), M is N//10,B is A*10+A1, rev(M,B,R).

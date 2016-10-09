e96a2dd9177c694def02e0a4b3ed0ec9b08b5127
ChaitanyaBaweja <chaitanya.baweja1@gmail.com>
2016-10-09 14:59:20 +0530

    Add files via upload

---
+++ Prolog/Prob 6.pl
@@ -0,0 +1,5 @@
+/* Program to reverse any given number.
+   Copyright (c) 2016 ChaitanyaBaweja
+*/
+/* proposed method is to covert the number into a list, reverse the list, convert it back into a number.
+*/

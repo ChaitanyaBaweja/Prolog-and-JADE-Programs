b08a2970e036ccafe4c7925b6e0344187c71e507
ChaitanyaBaweja <chaitanya.baweja1@gmail.com>
2016-10-09 14:31:19 +0530

    Add files via upload

--- Prolog/Prob 3.pl
+++ Prolog/Prob 3.pl
@@ -1,5 +1,5 @@
 fib(1,0).
 fib(2,1).
-fib(N,R):-write("0 1 "), fib(N,R,2,1,0).
+fib(N,R):-N>2, write("0 1 "), fib(N,R,2,1,0).
 fib(N,F2,N,F2,F1).
 fib(N,R,C,F2,F1):-F3 is F1+F2, C1 is C+1, write(F3),write(" "), fib(N,R,C1,F3,F2).
